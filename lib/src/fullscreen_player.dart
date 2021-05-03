c5a2675cb9675505aacf314bd66564b8054f27eb
Aram Semerjyan <semerjyan.aram.stdev@gmail.com>
2021-05-03 20:15:29 +0400

    Fix optional issue.

--- lib/src/fullscreen_player.dart
+++ lib/src/fullscreen_player.dart
@@ -1,10 +1,12 @@
 library vimeoplayer;
 
-import 'package:flutter/material.dart';
-import 'package:video_player/video_player.dart';
-import 'package:flutter/services.dart';
-import 'quality_links.dart';
 import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:video_player/video_player.dart';
+
+import 'quality_links.dart';
 
 //Класс видео плеера во весь экран
 class FullscreenPlayer extends StatefulWidget {
@@ -58,7 +60,7 @@ class _FullscreenPlayerState extends State<FullscreenPlayer> {
   bool _seek = true;
 
   // Overlay timer
-  Timer _timer;
+  Timer? _timer;
 
   //Переменные видео
   double? videoHeight;
@@ -113,167 +115,170 @@ class _FullscreenPlayerState extends State<FullscreenPlayer> {
     return WillPopScope(
         onWillPop: _onWillPop,
         child: Scaffold(
-          backgroundColor: Colors.black,
+            backgroundColor: Colors.black,
             body: Center(
                 child: Stack(
-          alignment: AlignmentDirectional.center,
-          children: <Widget>[
-            GestureDetector(
-              child: FutureBuilder(
-                  future: initFuture,
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.done) {
-                      //Управление шириной и высотой видео
-                      double delta = MediaQuery.of(context).size.width -
-                          MediaQuery.of(context).size.height *
-                              _controller!.value.aspectRatio;
-                      if (MediaQuery.of(context).orientation ==
-                              Orientation.portrait ||
-                          delta < 0) {
-                        videoHeight = MediaQuery.of(context).size.width /
-                            _controller!.value.aspectRatio;
-                        videoWidth = MediaQuery.of(context).size.width;
-                        videoMargin = 0;
-                      } else {
-                        videoHeight = MediaQuery.of(context).size.height;
-                        videoWidth =
-                            videoHeight! * _controller!.value.aspectRatio;
-                        videoMargin =
-                            (MediaQuery.of(context).size.width - videoWidth!) /
+              alignment: AlignmentDirectional.center,
+              children: <Widget>[
+                GestureDetector(
+                  child: FutureBuilder(
+                      future: initFuture,
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState == ConnectionState.done) {
+                          //Управление шириной и высотой видео
+                          double delta = MediaQuery.of(context).size.width -
+                              MediaQuery.of(context).size.height *
+                                  _controller!.value.aspectRatio;
+                          if (MediaQuery.of(context).orientation ==
+                                  Orientation.portrait ||
+                              delta < 0) {
+                            videoHeight = MediaQuery.of(context).size.width /
+                                _controller!.value.aspectRatio;
+                            videoWidth = MediaQuery.of(context).size.width;
+                            videoMargin = 0;
+                          } else {
+                            videoHeight = MediaQuery.of(context).size.height;
+                            videoWidth =
+                                videoHeight! * _controller!.value.aspectRatio;
+                            videoMargin = (MediaQuery.of(context).size.width -
+                                    videoWidth!) /
                                 2;
-                      }
-                      //Переменные дабл тапа, зависимые от размеров видео
-                      doubleTapRWidthFS = videoWidth;
-                      doubleTapRHeightFS = videoHeight! - 36;
-                      doubleTapLWidthFS = videoWidth;
-                      doubleTapLHeightFS = videoHeight;
+                          }
+                          //Переменные дабл тапа, зависимые от размеров видео
+                          doubleTapRWidthFS = videoWidth;
+                          doubleTapRHeightFS = videoHeight! - 36;
+                          doubleTapLWidthFS = videoWidth;
+                          doubleTapLHeightFS = videoHeight;
 
-                      //Сразу при входе в режим фуллскрин перематываем
-                      // на нужное место
-                      if (_seek && fullScreen) {
-                        _controller!.seekTo(Duration(seconds: position!));
-                        _seek = false;
-                      }
+                          //Сразу при входе в режим фуллскрин перематываем
+                          // на нужное место
+                          if (_seek && fullScreen) {
+                            _controller!.seekTo(Duration(seconds: position!));
+                            _seek = false;
+                          }
 
-                      //Переходи на нужное место при смене качества
-                      if (_seek && _controller!.value.duration.inSeconds > 2) {
-                        _controller!.seekTo(Duration(seconds: position!));
-                        _seek = false;
-                      }
-                      SystemChrome.setEnabledSystemUIOverlays(
-                          [SystemUiOverlay.bottom]);
+                          //Переходи на нужное место при смене качества
+                          if (_seek &&
+                              _controller!.value.duration.inSeconds > 2) {
+                            _controller!.seekTo(Duration(seconds: position!));
+                            _seek = false;
+                          }
+                          SystemChrome.setEnabledSystemUIOverlays(
+                              [SystemUiOverlay.bottom]);
 
-                      //Отрисовка элементов плеера
-                      return Stack(
-                        children: <Widget>[
-                          Container(
-                            height: videoHeight,
-                            width: videoWidth,
-                            margin: EdgeInsets.only(left: videoMargin),
-                            child: VideoPlayer(_controller!),
+                          //Отрисовка элементов плеера
+                          return Stack(
+                            children: <Widget>[
+                              Container(
+                                height: videoHeight,
+                                width: videoWidth,
+                                margin: EdgeInsets.only(left: videoMargin),
+                                child: VideoPlayer(_controller!),
+                              ),
+                              _videoOverlay(),
+                            ],
+                          );
+                        } else {
+                          return Center(
+                              heightFactor: 6,
+                              child: CircularProgressIndicator(
+                                strokeWidth: 4,
+                                valueColor: AlwaysStoppedAnimation<Color>(
+                                    Color(0xFF22A3D2)),
+                              ));
+                        }
+                      }),
+                  //Редактируем размер области дабл тапа при показе оверлея.
+                  // Сделано для открытия кнопок "Во весь экран" и "Качество"
+                  onTap: () {
+                    setState(() {
+                      _overlay = !_overlay;
+                      if (_overlay) {
+                        doubleTapRHeightFS = videoHeight! - 36;
+                        doubleTapLHeightFS = videoHeight! - 10;
+                        doubleTapRMarginFS = 36;
+                        doubleTapLMarginFS = 10;
+                      } else if (!_overlay) {
+                        doubleTapRHeightFS = videoHeight! + 36;
+                        doubleTapLHeightFS = videoHeight;
+                        doubleTapRMarginFS = 0;
+                        doubleTapLMarginFS = 0;
+                      }
+                    });
+                  },
+                ),
+                GestureDetector(
+                    child: Container(
+                      width: doubleTapLWidthFS! / 2 - 30,
+                      height: doubleTapLHeightFS! - 44,
+                      margin: EdgeInsets.fromLTRB(
+                          0, 0, doubleTapLWidthFS! / 2 + 30, 40),
+                      decoration: BoxDecoration(
+                          //color: Colors.red,
                           ),
-                          _videoOverlay(),
-                        ],
-                      );
-                    } else {
-                      return Center(
-                          heightFactor: 6,
-                          child: CircularProgressIndicator(
-                            strokeWidth: 4,
-                            valueColor: AlwaysStoppedAnimation<Color>(
-                                Color(0xFF22A3D2)),
-                          ));
-                    }
-                  }),
-              //Редактируем размер области дабл тапа при показе оверлея.
-              // Сделано для открытия кнопок "Во весь экран" и "Качество"
-              onTap: () {
-                setState(() {
-                  _overlay = !_overlay;
-                  if (_overlay) {
-                    doubleTapRHeightFS = videoHeight! - 36;
-                    doubleTapLHeightFS = videoHeight! - 10;
-                    doubleTapRMarginFS = 36;
-                    doubleTapLMarginFS = 10;
-                  } else if (!_overlay) {
-                    doubleTapRHeightFS = videoHeight! + 36;
-                    doubleTapLHeightFS = videoHeight;
-                    doubleTapRMarginFS = 0;
-                    doubleTapLMarginFS = 0;
-                  }
-                });
-              },
-            ),
-            GestureDetector(
-                child: Container(
-                  width: doubleTapLWidthFS! / 2 - 30,
-                  height: doubleTapLHeightFS! - 44,
-                  margin:
-                      EdgeInsets.fromLTRB(0, 0, doubleTapLWidthFS! / 2 + 30, 40),
-                  decoration: BoxDecoration(
-                    //color: Colors.red,
-                  ),
-                ),
-                //Редактируем размер области дабл тапа при показе оверлея.
-                // Сделано для открытия кнопок "Во весь экран" и "Качество"
-                onTap: () {
-                  setState(() {
-                    _overlay = !_overlay;
-                    if (_overlay) {
-                      doubleTapRHeightFS = videoHeight! - 36;
-                      doubleTapLHeightFS = videoHeight! - 10;
-                      doubleTapRMarginFS = 36;
-                      doubleTapLMarginFS = 10;
-                    } else if (!_overlay) {
-                      doubleTapRHeightFS = videoHeight! + 36;
-                      doubleTapLHeightFS = videoHeight;
-                      doubleTapRMarginFS = 0;
-                      doubleTapLMarginFS = 0;
-                    }
-                  });
-                },
-                onDoubleTap: () {
-                  setState(() {
-                    _controller!.seekTo(Duration(
-                        seconds: _controller!.value.position.inSeconds - 10));
-                  });
-                }),
-            GestureDetector(
-                child: Container(
-                  width: doubleTapRWidthFS! / 2 - 45,
-                  height: doubleTapRHeightFS - 80,
-                  margin: EdgeInsets.fromLTRB(doubleTapRWidthFS! / 2 + 45, 0, 0,
-                      doubleTapLMarginFS + 20),
-                  decoration: BoxDecoration(
-                    //color: Colors.red,
-                  ),
-                ),
-                //Редактируем размер области дабл тапа при показе оверлея.
-                // Сделано для открытия кнопок "Во весь экран" и "Качество"
-                onTap: () {
-                  setState(() {
-                    _overlay = !_overlay;
-                    if (_overlay) {
-                      doubleTapRHeightFS = videoHeight! - 36;
-                      doubleTapLHeightFS = videoHeight! - 10;
-                      doubleTapRMarginFS = 36;
-                      doubleTapLMarginFS = 10;
-                    } else if (!_overlay) {
-                      doubleTapRHeightFS = videoHeight! + 36;
-                      doubleTapLHeightFS = videoHeight;
-                      doubleTapRMarginFS = 0;
-                      doubleTapLMarginFS = 0;
-                    }
-                  });
-                },
-                onDoubleTap: () {
-                  setState(() {
-                    _controller!.seekTo(Duration(
-                        seconds: _controller!.value.position.inSeconds + 10));
-                  });
-                }),
-          ],
-        ))));
+                    ),
+                    //Редактируем размер области дабл тапа при показе оверлея.
+                    // Сделано для открытия кнопок "Во весь экран" и "Качество"
+                    onTap: () {
+                      setState(() {
+                        _overlay = !_overlay;
+                        if (_overlay) {
+                          doubleTapRHeightFS = videoHeight! - 36;
+                          doubleTapLHeightFS = videoHeight! - 10;
+                          doubleTapRMarginFS = 36;
+                          doubleTapLMarginFS = 10;
+                        } else if (!_overlay) {
+                          doubleTapRHeightFS = videoHeight! + 36;
+                          doubleTapLHeightFS = videoHeight;
+                          doubleTapRMarginFS = 0;
+                          doubleTapLMarginFS = 0;
+                        }
+                      });
+                    },
+                    onDoubleTap: () {
+                      setState(() {
+                        _controller!.seekTo(Duration(
+                            seconds:
+                                _controller!.value.position.inSeconds - 10));
+                      });
+                    }),
+                GestureDetector(
+                    child: Container(
+                      width: doubleTapRWidthFS! / 2 - 45,
+                      height: doubleTapRHeightFS - 80,
+                      margin: EdgeInsets.fromLTRB(doubleTapRWidthFS! / 2 + 45,
+                          0, 0, doubleTapLMarginFS + 20),
+                      decoration: BoxDecoration(
+                          //color: Colors.red,
+                          ),
+                    ),
+                    //Редактируем размер области дабл тапа при показе оверлея.
+                    // Сделано для открытия кнопок "Во весь экран" и "Качество"
+                    onTap: () {
+                      setState(() {
+                        _overlay = !_overlay;
+                        if (_overlay) {
+                          doubleTapRHeightFS = videoHeight! - 36;
+                          doubleTapLHeightFS = videoHeight! - 10;
+                          doubleTapRMarginFS = 36;
+                          doubleTapLMarginFS = 10;
+                        } else if (!_overlay) {
+                          doubleTapRHeightFS = videoHeight! + 36;
+                          doubleTapLHeightFS = videoHeight;
+                          doubleTapRMarginFS = 0;
+                          doubleTapLMarginFS = 0;
+                        }
+                      });
+                    },
+                    onDoubleTap: () {
+                      setState(() {
+                        _controller!.seekTo(Duration(
+                            seconds:
+                                _controller!.value.position.inSeconds + 10));
+                      });
+                    }),
+              ],
+            ))));
   }
 
   //================================ Quality ================================//
@@ -307,8 +312,7 @@ class _FullscreenPlayerState extends State<FullscreenPlayer> {
 
   //================================ OVERLAY ================================//
   Widget _videoOverlay() {
-
-    if (_timer != null) _timer.cancel();
+    if (_timer != null) _timer!.cancel();
 
     _timer = Timer.periodic(Duration(seconds: 3), (timer) {
       timer.cancel();
@@ -358,7 +362,8 @@ class _FullscreenPlayerState extends State<FullscreenPlayer> {
               ),
               Container(
                 margin: EdgeInsets.only(
-                    top: videoHeight! - 80, left: videoWidth! + videoMargin - 50),
+                    top: videoHeight! - 80,
+                    left: videoWidth! + videoMargin - 50),
                 child: IconButton(
                     alignment: AlignmentDirectional.center,
                     icon: Icon(Icons.fullscreen, size: 30.0),
@@ -447,7 +452,7 @@ class _FullscreenPlayerState extends State<FullscreenPlayer> {
 
   @override
   void dispose() {
-    _timer.cancel();
+    _timer!.cancel();
 
     super.dispose();
   }
